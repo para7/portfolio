2cac74d225b286104f980db4f06e9c95bcde662d
para7 <cometpara7@gmail.com>
2024-05-01 10:15:15 +0900

    setup ladle & mantine

---
+++ remix/src/components/global/Footer/AppHeader.stories.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "./Footer";
+
+import type { Story, StoryDefault } from "@ladle/react";
+
+export default {
+  title: "components/global/footer",
+} satisfies StoryDefault;
+
+export const Story1: Story = () => {
+  return <Footer />;
+};
