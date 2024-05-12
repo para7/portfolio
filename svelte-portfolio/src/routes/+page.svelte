59227152d60b57b06d72d95705ff64f8eec1b6d2
para7 <cometpara7@gmail.com>
2024-05-12 22:22:21 +0900

    仕様調べたらエイリアス不要だったのでrevert

--- svelte-portfolio/src/routes/+page.svelte
+++ svelte-portfolio/src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Counter from "./Counter.svelte";
   import welcome from "$lib/images/svelte-welcome.webp";
   import welcome_fallback from "$lib/images/svelte-welcome.png";
-    import Card from "@/components/card.svelte";
+    import Card from "$lib/components/card.svelte";
 </script>
 
 <svelte:head>
